a32f2430a414b98eaae4f9b85b38ef75ae9ef080
akkikyu <mmancebo@ctsformacion.com>
2025-05-16 13:57:02 +0200

    busteo en el canva de game over

---
+++ Assets/Scripts/text Animation.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class textAnimation : MonoBehaviour
+{
+
+public GameObject boton;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ActivarBotones()
+    {
+        boton.SetActive(true);
+    } 
+}
