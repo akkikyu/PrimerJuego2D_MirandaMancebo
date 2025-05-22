1df19e8504c4658d49801678f03b485951229e4a
akkikyu <mmancebo@ctsformacion.com>
2025-05-22 14:04:10 +0200

    inicio start menu

---
+++ Assets/Scripts/StartManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartManager : MonoBehaviour
+{
+    /*public void Play()
+    {
+        SceneManagement.LoadScene(1); 
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }*/
+}
